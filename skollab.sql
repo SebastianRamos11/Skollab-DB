f1983a1759bffb68ae2004ee4eae1c41d01c9716
unknown <sebasti.ramos08@gmail.com>
2023-03-17 16:04:14 -0500

    More insertions applied

--- skollab.sql
+++ skollab.sql
@@ -73,14 +73,14 @@ CREATE TABLE `ambiente_virtual` (
 --
 
 INSERT INTO `ambiente_virtual` (`ID_Persona`, `ID_Ficha`) VALUES
-(2, 1),
-(3, 1),
-(4, 1),
-(5, 1),
-(6, 1),
-(7, 1),
-(8, 1),
-(9, 1);
+(2, 1), (3, 1), (4, 1), (5, 1),
+(6, 1), (7, 1), (8, 1), (9, 1),
+(10, 1), (11, 1), (12, 1), (13, 1),
+(14, 1), (15, 1), (16, 1), (17, 1),
+(18, 1), (19, 1), (20, 1), (21, 1),
+(22, 1), (23, 1), (24, 1), (25, 1),
+(26, 1), (27, 1), (28, 1);
+
 
 -- --------------------------------------------------------
 
@@ -99,8 +99,14 @@ CREATE TABLE `curso` (
 --
 
 INSERT INTO `curso` (`ID_Ficha`, `ID_Materia`, `ID_Instructor`) VALUES
-(1, 1, 3),
-(1, 2, 2);
+(1, 1, 2),
+(1, 2, 3),
+(1, 3, 4),
+(1, 4, 5),
+(1, 5, 6),
+(1, 6, 7),
+(1, 7, 8);
+
 
 -- --------------------------------------------------------
 
@@ -137,8 +143,8 @@ CREATE TABLE `ficha` (
 -- Volcado de datos para la tabla `ficha`
 --
 
-INSERT INTO `ficha` (`ID_Ficha`, `numero`, `codigo`) VALUES
-(1, 1101, 2022);
+INSERT INTO `ficha` (`ID_Ficha`, `numero`, `descripcion`, `codigo`) VALUES
+(1, 1101, 'En el grado once de secundaria, los estudiantes continúan con su educación en una amplia variedad de asignaturas, incluyendo matemáticas avanzadas, ciencias, estudios sociales, inglés, idiomas extranjeros y arte y música. Los estudiantes pueden aprender sobre temas como álgebra avanzada, geometría, trigonometría, biología, química, física, historia, geografía, economía, política, literatura, gramática, pronunciación y análisis crítico de obras de arte y música. En general, el grado once es un año importante en la educación secundaria en el que los estudiantes se preparan para la universidad o la educación postsecundaria.', 2022);
 
 -- --------------------------------------------------------
 
@@ -166,12 +172,31 @@ INSERT INTO `persona` (`ID_Persona`, `num_documento`, `ID_Tipo_Documento`, `nomb
 (1, 1010, 1, 'ADMINISTRADOR', 'AMBIENTE', 1, 'admin@skollab.com', 'a543e8fcda2f0b318826667effe4e4b3ac22705112f9470d7e3d07c71ca1025d235e6d361160b5b2b0b61f7121b84234dd0ddbd00d206f8f4805fef04f595193', 1937248522),
 (2, 124059870, 1, 'CAROLINA', 'PINZON GAITAN', 2, 'cpinzon@skollab.com', '3627909a29c31381a071ec27f7c9ca97726182aed29a7ddd2e54353322cfb30abb9e3a6df2ac2c20fe23436311d678564d0c8d305930575f60e2d3d048184d79', 3938128394),
 (3, 524059870, 1, 'EDWIN DAVID', 'ROA PÉREZ', 2, 'eroa@skollab.com', '3627909a29c31381a071ec27f7c9ca97726182aed29a7ddd2e54353322cfb30abb9e3a6df2ac2c20fe23436311d678564d0c8d305930575f60e2d3d048184d79', 1385748391),
-(4, 1328403813, 1, 'ANDRES SEBASTIAN', 'RAMOS MARTINEZ', 3, 'aramos@skollab.com', '3627909a29c31381a071ec27f7c9ca97726182aed29a7ddd2e54353322cfb30abb9e3a6df2ac2c20fe23436311d678564d0c8d305930575f60e2d3d048184d79', 1948503912),
-(5, 1023958301, 1, 'JOHAN DAVID', 'PÉREZ VELOSA', 3, 'jperez@skollab.com', '3627909a29c31381a071ec27f7c9ca97726182aed29a7ddd2e54353322cfb30abb9e3a6df2ac2c20fe23436311d678564d0c8d305930575f60e2d3d048184d79', 3981238456),
-(6, 1049375903, 1, 'CRISTIAN ALEJANDRO', 'CUESTA', 3, 'ccuesta@skollab.com', '3627909a29c31381a071ec27f7c9ca97726182aed29a7ddd2e54353322cfb30abb9e3a6df2ac2c20fe23436311d678564d0c8d305930575f60e2d3d048184d79', 3020123023),
-(7, 1948302847, 1, 'NICOLAS JAVIER', 'LARA BONILLA', 3, 'nlara@skollab.com', '3627909a29c31381a071ec27f7c9ca97726182aed29a7ddd2e54353322cfb30abb9e3a6df2ac2c20fe23436311d678564d0c8d305930575f60e2d3d048184d79', 1319412341),
-(8, 1029385749, 2, 'JUAN DARÍO', 'TORRES', 3, 'jtorres@skollab.com', '3627909a29c31381a071ec27f7c9ca97726182aed29a7ddd2e54353322cfb30abb9e3a6df2ac2c20fe23436311d678564d0c8d305930575f60e2d3d048184d79', 2013940123),
-(9, 1204932912, 2, 'PEDRO ALEXANDER', 'CASTILLO', 3, 'pcastillo@skollab.com', '3627909a29c31381a071ec27f7c9ca97726182aed29a7ddd2e54353322cfb30abb9e3a6df2ac2c20fe23436311d678564d0c8d305930575f60e2d3d048184d79', 2948203948);
+(4, 8391039283, 1, 'LUIS FELIPE', 'VÉLEZ ORTÍZ', 2, 'lvelez@skollab.com', '3627909a29c31381a071ec27f7c9ca97726182aed29a7ddd2e54353322cfb30abb9e3a6df2ac2c20fe23436311d678564d0c8d305930575f60e2d3d048184d79', 3216936714),
+(5, 193827492, 1, 'LUIS CARLOS', 'PINILLA', 2, 'lpinilla@skollab.com', '3627909a29c31381a071ec27f7c9ca97726182aed29a7ddd2e54353322cfb30abb9e3a6df2ac2c20fe23436311d678564d0c8d305930575f60e2d3d048184d79', 3209637418),
+(6, 8492019293, 1, 'RAFAEL ANTONIO', 'COLÓN', 2, 'rcolon@skollab.com', '3627909a29c31381a071ec27f7c9ca97726182aed29a7ddd2e54353322cfb30abb9e3a6df2ac2c20fe23436311d678564d0c8d305930575f60e2d3d048184d79', 3107465953),
+(7, 37281932, 1, 'HECTOR ABAD', 'LÓPEZ MARTÍNEZ', 2, 'hlopez@skollab.com', '3627909a29c31381a071ec27f7c9ca97726182aed29a7ddd2e54353322cfb30abb9e3a6df2ac2c20fe23436311d678564d0c8d305930575f60e2d3d048184d79', 3265102580),
+(8, 5983169547, 1, 'MARIO ALFREDO', 'MENDOZA ÁVILA', 2, 'mmendoza@skollab.com', '3627909a29c31381a071ec27f7c9ca97726182aed29a7ddd2e54353322cfb30abb9e3a6df2ac2c20fe23436311d678564d0c8d305930575f60e2d3d048184d79', 2106326598),
+(9, 1328403813, 1, 'ANDRES SEBASTIAN', 'RAMOS MARTINEZ', 3, 'aramos@skollab.com', '3627909a29c31381a071ec27f7c9ca97726182aed29a7ddd2e54353322cfb30abb9e3a6df2ac2c20fe23436311d678564d0c8d305930575f60e2d3d048184d79', 1948503912),
+(10, 1023958301, 1, 'JOHAN DAVID', 'PÉREZ VELOSA', 3, 'jperez@skollab.com', '3627909a29c31381a071ec27f7c9ca97726182aed29a7ddd2e54353322cfb30abb9e3a6df2ac2c20fe23436311d678564d0c8d305930575f60e2d3d048184d79', 3981238456),
+(11, 1049375903, 1, 'CRISTIAN ALEJANDRO', 'CUESTA', 3, 'ccuesta@skollab.com', '3627909a29c31381a071ec27f7c9ca97726182aed29a7ddd2e54353322cfb30abb9e3a6df2ac2c20fe23436311d678564d0c8d305930575f60e2d3d048184d79', 3020123023),
+(12, 1948302847, 1, 'NICOLAS JAVIER', 'LARA BONILLA', 3, 'nlara@skollab.com', '3627909a29c31381a071ec27f7c9ca97726182aed29a7ddd2e54353322cfb30abb9e3a6df2ac2c20fe23436311d678564d0c8d305930575f60e2d3d048184d79', 1319412341),
+(13, 1029385749, 2, 'JUAN DARÍO', 'TORRES', 3, 'jtorres@skollab.com', '3627909a29c31381a071ec27f7c9ca97726182aed29a7ddd2e54353322cfb30abb9e3a6df2ac2c20fe23436311d678564d0c8d305930575f60e2d3d048184d79', 2013940123),
+(14, 1204932912, 2, 'PEDRO ALEXANDER', 'CASTILLO', 3, 'pcastillo@skollab.com', '3627909a29c31381a071ec27f7c9ca97726182aed29a7ddd2e54353322cfb30abb9e3a6df2ac2c20fe23436311d678564d0c8d305930575f60e2d3d048184d79', 2948203948),
+(15, 1503469710, 1, 'JUAN SEBASTIAN', 'GONZALES PEÑA', 2, 'jgonzales@skollab.com', '3627909a29c31381a071ec27f7c9ca97726182aed29a7ddd2e54353322cfb30abb9e3a6df2ac2c20fe23436311d678564d0c8d305930575f60e2d3d048184d79', 3019482031),
+(16, 1021598364, 1, 'GEORGE', 'DE LA ROSA', 2, 'gdelarosa@skollab.com', '3627909a29c31381a071ec27f7c9ca97726182aed29a7ddd2e54353322cfb30abb9e3a6df2ac2c20fe23436311d678564d0c8d305930575f60e2d3d048184d79', 3112940293),
+(17, 1014025010, 1, 'FLORENTINO', 'CHAPARRO', 2, 'fchaparro@skollab.com', '3627909a29c31381a071ec27f7c9ca97726182aed29a7ddd2e54353322cfb30abb9e3a6df2ac2c20fe23436311d678564d0c8d305930575f60e2d3d048184d79', 3124902813),
+(18, 1010102369, 1, 'JORGE', 'VAQUERO', 2, 'jvaquero@skollab.com', '3627909a29c31381a071ec27f7c9ca97726182aed29a7ddd2e54353322cfb30abb9e3a6df2ac2c20fe23436311d678564d0c8d305930575f60e2d3d048184d79', 3124567416),
+(19, 2135036879, 1, 'ADOLFO', 'CARDONA', 2, 'acardona@skollab.com', '3627909a29c31381a071ec27f7c9ca97726182aed29a7ddd2e54353322cfb30abb9e3a6df2ac2c20fe23436311d678564d0c8d305930575f60e2d3d048184d79', 3201699658),
+(20, 1012023694, 1, 'DANNA VALENTINA', 'LUCENA', 2, 'dlucena@skollab.com', '3627909a29c31381a071ec27f7c9ca97726182aed29a7ddd2e54353322cfb30abb9e3a6df2ac2c20fe23436311d678564d0c8d305930575f60e2d3d048184d79', 3216593658),
+(21, 1012470031, 1, 'SOFIA', 'VELOZA ANIZ', 2, 'sveloza@skollab.com', '3627909a29c31381a071ec27f7c9ca97726182aed29a7ddd2e54353322cfb30abb9e3a6df2ac2c20fe23436311d678564d0c8d305930575f60e2d3d048184d79', 3216336598),
+(22, 1056315974, 1, 'VIOLETA', 'CASTILLO', 2, 'vcastillo@skollab.com', '3627909a29c31381a071ec27f7c9ca97726182aed29a7ddd2e54353322cfb30abb9e3a6df2ac2c20fe23436311d678564d0c8d305930575f60e2d3d048184d79', 3215414120),
+(23, 1021587102, 1, 'SARA MARIA', 'AZNAR', 2, 'saznar@skollab.com', '3627909a29c31381a071ec27f7c9ca97726182aed29a7ddd2e54353322cfb30abb9e3a6df2ac2c20fe23436311d678564d0c8d305930575f60e2d3d048184d79', 3102654893),
+(24, 1458102201, 1, 'XAVIER', 'SALGADO', 2, 'xsalgado@skollab.com', '3627909a29c31381a071ec27f7c9ca97726182aed29a7ddd2e54353322cfb30abb9e3a6df2ac2c20fe23436311d678564d0c8d305930575f60e2d3d048184d79', 3403659721),
+(25, 1002544701, 1, 'JOSEPH', 'RODRIGUEZ', 2, 'jrodriguez@skollab.com', '3627909a29c31381a071ec27f7c9ca97726182aed29a7ddd2e54353322cfb30abb9e3a6df2ac2c20fe23436311d678564d0c8d305930575f60e2d3d048184d79', 1236974125),
+(26, 1123614782, 1, 'FERMIN', 'VELASCO', 2, 'jvelasco@skollab.com', '3627909a29c31381a071ec27f7c9ca97726182aed29a7ddd2e54353322cfb30abb9e3a6df2ac2c20fe23436311d678564d0c8d305930575f60e2d3d048184d79', 3216549800),
+(27, 1012464797, 1, 'TYRONE', 'GONZALES', 2, 'tgonzales@skollab.com', '3627909a29c31381a071ec27f7c9ca97726182aed29a7ddd2e54353322cfb30abb9e3a6df2ac2c20fe23436311d678564d0c8d305930575f60e2d3d048184d79', 32974355102),
+(28, 2023202452, 1, 'NICOLAE', 'MANCILLA', 2, 'nmancilla@skollab.com', '3627909a29c31381a071ec27f7c9ca97726182aed29a7ddd2e54353322cfb30abb9e3a6df2ac2c20fe23436311d678564d0c8d305930575f60e2d3d048184d79', 2136549714);
 -- --------------------------------------------------------
 
 --
@@ -189,8 +214,13 @@ CREATE TABLE `materia` (
 --
 
 INSERT INTO `materia` (`ID_Materia`, `nombre`, `img`) VALUES
-(1, 'ANÁLISIS Y DESARROLLO DE SISTEMAS DE INFORMACIÓN', "../file-store/subjects/sena-logo.png"),
-(2, 'MATEMÁTICAS', "../file-store/subjects/sena-logo.png");
+(1, 'ESPAÑOL', "../file-store/subjects/school_logo.png"),
+(2, 'MATEMÁTICAS', "../file-store/subjects/school_logo.png"),
+(3, 'SOCIALES', "../file-store/subjects/school_logo.png"),
+(4, 'INGLÉS', "../file-store/subjects/school_logo.png"),
+(5, 'FÍSICA', "../file-store/subjects/school_logo.png"),
+(6, 'QUÍMICA', "../file-store/subjects/school_logo.png"),
+(7, 'FILOSOFÍA', "../file-store/subjects/school_logo.png");
 
 -- --------------------------------------------------------
 --
